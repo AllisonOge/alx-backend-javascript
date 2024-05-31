efaaef8f27482d158bd0fe7a892df49c46468338
Allison Ogechukwu <ogechukwukanu@gmail.com>
2024-05-31 01:48:32 +0000

    task#5: a building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+export default class Building {
+  constructor(sqft) {
+    if (new.target !== Building
+        && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+
+  set sqft(newSqft) {
+    this._sqft = newSqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}

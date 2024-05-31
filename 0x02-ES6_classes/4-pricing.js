c13c539437702fcd77f3cd4bd9cd855a99a3c96a
Allison Ogechukwu <ogechukwukanu@gmail.com>
2024-05-31 01:35:52 +0000

    task#4: pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,28 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+}

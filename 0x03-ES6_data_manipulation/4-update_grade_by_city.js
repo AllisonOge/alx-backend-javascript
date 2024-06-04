6c406585d8a8709b278382862bc13d88994db99f
Allison Ogechukwu <ogechukwukanu@gmail.com>
2024-06-04 10:36:53 +0000

    task#4: combine

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,10 @@
+export default function updateStudentGraderByCity(listOfStudents, city, newGrades) {
+  return Array.isArray(listOfStudents) ? listOfStudents.filter((student) => student.location == city)
+    .map((obj) => {
+      const newGrade = Array.isArray(newGrades) ? newGrades.filter(({ id }) => id == obj.id) : [];
+
+      if (newGrade.length > 0)
+        return {...obj, grade: newGrade[0] };
+      return { ...obj, grade: 'N/A' };
+    }) : [];
+}

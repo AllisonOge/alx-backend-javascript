9389d8da2762914b35705626850fd5497ab8bb3e
Allison Ogechukwu <ogechukwukanu@gmail.com>
2024-06-05 03:06:04 +0000

    task#8: clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,8 @@
+export default function cleanSet(set, startString) {
+  if (!startString) {
+    return '';
+  }
+  return Array.from(set).filter((str) => str && str.startsWith(startString))
+    .map((str) => str.replace(startString, ''))
+    .join('-');
+}

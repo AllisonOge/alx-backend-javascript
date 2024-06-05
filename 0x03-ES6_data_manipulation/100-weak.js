bcbb9fea4aee3d41e87e8a8c62e56b562b01dd83
Allison Ogechukwu <ogechukwukanu@gmail.com>
2024-06-05 03:27:17 +0000

    task#11: weak link data structure

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,7 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  const counter = (weakMap.get(endpoint) || 0) + 1;
+  if (counter >= 5) throw new Error('Endpoint load is high');
+  weakMap.set(endpoint, counter);
+}

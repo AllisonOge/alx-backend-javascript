023379d65d69fee148258dd4ac08ce99088f27a5
Allison Ogechukwu <ogechukwukanu@gmail.com>
2024-07-26 04:03:57 +0000

    task#3: using sinon spies

---
+++ 0x06-unittests_in_js/3-payments.test.js
@@ -0,0 +1,12 @@
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./3-payments');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', function () {
+  it('should call Utils.calculateNumber', function () {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(100, 20);
+    sinon.assert.calledWith(spy, 'SUM', 100, 20);
+    spy.restore();
+  });
+});

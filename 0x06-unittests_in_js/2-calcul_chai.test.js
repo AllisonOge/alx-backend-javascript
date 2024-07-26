05623b75b808cfea8b22fc56cc307a80ec6806d6
Allison Ogechukwu <ogechukwukanu@gmail.com>
2024-07-26 03:25:36 +0000

    task#2: chai basic test

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,27 @@
+const expect = require('chai').expect;
+const calculateNumber = require('./2-calcul');
+
+
+describe('calculateNumber', function () {
+  it('should return 6 given SUM, 1.4 and 4.5 as argument', function () {
+    expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+  });
+});
+
+describe('calculateNumber', function () {
+  it('should return -4 given SUBTRACT, 1.4 and 4.5 as argument', function () {
+    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+  });
+});
+
+describe('calculateNumber', function () {
+  it('should return 0.2 given DIVIDE, 1.4 and 4.5 as argument', function () {
+    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+  });
+});
+
+describe('calculateNumber', function () {
+  it('should return 6 given Error, 1.4 and 0 as argument', function () {
+    expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+  });
+});
